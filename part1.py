10a31cfc7f9ff48d0de80152e2394abf136a7482
Oleg Ligay <ligayoleg@gmail.com>
2020-06-15 18:32:39 -0400

    added part1

---
+++ part1.py
@@ -0,0 +1,33 @@
+#import libraries
+import requests
+from bs4 import BeautifulSoup
+
+# define base url
+base_url = r"https://www.sec.gov/Archives/edgar/data"
+
+# define cik
+cik_num = '/21344/'
+
+# create filling link
+fillings_url = base_url + cik_num + '/index.json'
+
+# request the url
+content = requests.get(fillings_url)
+decoded_content = content.json()
+
+# go and grab a single filing number
+filing_number = decoded_content['directory']['item'][0]['name']
+
+# define filing number url
+filling_url = base_url + cik_num + filing_number + '/index.json'
+
+# request the url
+content = requests.get(filling_url)
+doc_content = content.json()
+
+for doc in doc_content['directory']['item']:
+    if doc['type'] != 'image2.gif':
+
+        doc_name = doc['name']
+        doc_url = base_url + cik_num + filing_number + '/' + doc_name
+        print(doc_url)
